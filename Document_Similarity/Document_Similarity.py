4040b72b7f228a9698a875257aacf006c3718431
Nikos Bakalis <47317522+NikosBakalis@users.noreply.github.com>
2019-05-01 02:09:11 +0300

    Update Document_Similarity.py

--- Document_Similarity/Document_Similarity.py
+++ Document_Similarity/Document_Similarity.py
@@ -1,10 +1,17 @@
 from collections import Counter
 from itertools import chain
+import math
 
 N = int(input("Amount of documents: "))
 listsOne = ["Null"] * N
 listTwo = list()
 dictToList = list()
+digitList = list()
+resultList = list()
+part1 = 0
+part2 = 0
+part3 = 0
+cos = 0
 
 for x in range(N):
     print("Document No.", x + 1)
@@ -58,3 +65,47 @@ for y in range(len(chunks)):
 
 for lists in range(N):
     print(listsOne[lists], sep="\n")
+    for micro in range(len(listsOne[lists])):
+        #        print(listsOne[lists][micro])
+        for value in range(len(listsOne[lists][micro])):
+            #            print(listsOne[lists][micro][1])
+            digitList.append(listsOne[lists][micro][1])
+            break
+
+print("\n\n", digitList, "\n")
+
+digitChunks = [digitList[x:x+(int(len(digitList)/N))] for x in range(0, len(digitList), int(len(digitList)/N))]
+for allChunks in range(len(digitChunks)):
+    print(digitChunks[allChunks], sep="\n")
+
+for allChunks in range(len(digitChunks)):
+    for allDigits in range(len(digitChunks[allChunks])):
+        print(digitChunks[allChunks][allDigits])
+    print("\n")
+
+
+for i in range(len(digitChunks)):
+    for j in range(i + 1, len(digitChunks)):
+        for allDigits in range(len(digitChunks[i])):
+            print(digitChunks[i][allDigits], digitChunks[j][allDigits])
+            part1 = float(part1 + (digitChunks[i][allDigits] * digitChunks[j][allDigits]))
+            print("The part1 between list No: ", i + 1, "and list No: ", j + 1, "is: ", part1)
+            part2 = float(part2 + (digitChunks[i][allDigits] * digitChunks[i][allDigits]))
+            print("The part2 between list No: ", i + 1, "and list No: ", j + 1, "is: ", part2)
+            part3 = float(part3 + (digitChunks[j][allDigits] * digitChunks[j][allDigits]))
+            print("The part3 between list No: ", i + 1, "and list No: ", j + 1, "is: ", part3)
+        cos = float(part1 / (math.sqrt(part2) * math.sqrt(part3)))
+        print("The cos between list No: ", i + 1, "and list No: ", j + 1, "is: ", cos)
+        resultList.append(i + 1)
+        resultList.append(j + 1)
+        resultList.append(cos)
+        print(resultList)
+        part1 = 0
+        part2 = 0
+        part3 = 0
+        cos = 0
+        print("\n")
+
+resultChunks = [resultList[x:x+(int(len(resultList)/N))] for x in range(0, len(resultList), int(len(resultList)/N))]
+for allResults in range(len(resultChunks)):
+    print(resultChunks[allResults], sep="\n")
